5c894302863ea38241a952398a296aab29ed654c
Philip McGuire <coderphilip@gmail.com>
2021-10-15 12:12:28 -0400

    add testing lib and two blank tests

---
+++ test/com/pmcguire/cmsc335/trafficsim/gui/CarsDisplayTest.java
@@ -0,0 +1,22 @@
+package com.pmcguire.cmsc335.trafficsim.gui;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarsDisplayTest
+{
+
+    @Test
+    void addCarView()
+    {
+        // Create simple window
+
+        // Create car view
+
+        // Add car view to window
+
+        // Confirm this is a valid GUI
+
+    }
+}
---
+++ test/com/pmcguire/cmsc335/trafficsim/gui/LightsDisplayTest.java
@@ -0,0 +1,23 @@
+package com.pmcguire.cmsc335.trafficsim.gui;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LightsDisplayTest
+{
+    @Test
+    void addLightView()
+    {
+        // Create simple window
+
+        // Create light view
+
+        // Add light view to window
+
+        // Confirm this is a valid GUI
+
+    }
+}
